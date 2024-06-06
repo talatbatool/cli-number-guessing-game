1aaefc3f0d729b345f33c7064f8777774988af9a
talatbatool <166723175+talatbatool@users.noreply.github.com>
2024-06-05 18:31:00 -0700

    no guessing game

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 #! usr/bin/env node
 import inquirer from "inquirer";
-const randomNumber = Math.floor(Math.random() * 10 + 1);
+const randomNumber = Math.floor(Math.random() * 6 + 1);
 console.log(randomNumber);
 const answers = await inquirer.prompt([
     {
