6b6c257820878c6c35d3683649e0c45554eb859f
talatbatool <166723175+talatbatool@users.noreply.github.com>
2024-06-05 18:07:47 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log(randomNumber);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number 1-6:",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulation! you guesses right number.");
+}
+else {
+    console.log("you guessed wrong number");
+}
